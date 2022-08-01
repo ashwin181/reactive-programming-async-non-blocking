8ebc3afe9bb40a73fddce917bcc610f12779ba4e
ashwin181 <raiashwin018@gmail.com>
2022-07-31 21:18:37 -0400

    Update: Spring Reactive Streams Done

---
+++ src/test/java/com/ashwin/webflux/SpringbootWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashwin.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
